ea4f99e9feb8dfafb543f9b703bb1e22af73154a
Raziel Melchor <razielmelchor@gmail.com>
2021-10-10 17:57:21 -0500

    starting commit

---
+++ block.py
@@ -0,0 +1,18 @@
+class Block:
+    """
+    Block: a unit of storage.
+    Store transactions in a blockchain that supports a cryptocurrency
+    """
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return f'Block - data: {self.data}'
+
+def main():
+    block = Block('foo')
+    print(block)
+
+if __name__ == '__main__':
+    main()
